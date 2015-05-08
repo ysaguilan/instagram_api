e06f587a3c16f0c3e6c7b11919dfbba048ce537b
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-05-08 09:13:07 -0700

    Milestone 4: - Fixed RedirectURI error "_" Couldn't be Used in Define - Fixed Misspelling of Oauth

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //Make Constants Using Define
-define('client_ID', '7b50e23612434a658ce667c6e0ac3b74');
-define('client_Secret', 'f1795294356d41468a7970fd9e4147d0');
+define('clientID', '7b50e23612434a658ce667c6e0ac3b74');
+define('clientSecret', 'f1795294356d41468a7970fd9e4147d0');
 define('redirectURI', 'http://localhost/appacademyapi/index.php');
 define('ImageDirectory', 'pics/');
 
@@ -20,8 +20,8 @@ define('ImageDirectory', 'pics/');
 </head>
 <body>
 <!-- Creating a Login for People to go and Give Approval for Our Web App to Access Their Instagram Account
-After Getting Approval we are now Goingto Have the Informationso That we can Play With it
+After Getting Approval we are now Goingto Have the Information so That we can Play With it
  -->
-<a href="https://api.instagram/ouath/authorize/?client_id=<?php echo client_ID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">LOGIN</a>
+<a href="https:api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">LOGIN</a>
 </body>
 </html>
