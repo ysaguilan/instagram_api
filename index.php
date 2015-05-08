a76700e02aa8b7e57031181bd9cb5103a1e00b29
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-05-08 08:33:08 -0700

    Milestone 3: - Created a Login for People to go and Give Approval for Our Web App to Access Their Instagram Account - Error Does Not Redirect to Instagram Page

--- index.php
+++ index.php
@@ -6,15 +6,22 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //Make Constants Using Define
-define('client_id', '7b50e23612434a658ce667c6e0ac3b74');
-define('client_secret', 'f1795294356d41468a7970fd9e4147d0');
+define('client_ID', '7b50e23612434a658ce667c6e0ac3b74');
+define('client_Secret', 'f1795294356d41468a7970fd9e4147d0');
 define('redirectURI', 'http://localhost/appacademyapi/index.php');
 define('ImageDirectory', 'pics/');
 
  ?>
-<!-- 
-CLIENT INFO
-CLIENT ID	7b50e23612434a658ce667c6e0ac3b74
-CLIENT SECRET	f1795294356d41468a7970fd9e4147d0
-WEBSITE URL	http://localhost/appacademyapi/index.php
-REDIRECT URI	http://localhost/appacademyapi/index.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<!-- Creating a Login for People to go and Give Approval for Our Web App to Access Their Instagram Account
+After Getting Approval we are now Goingto Have the Informationso That we can Play With it
+ -->
+<a href="https://api.instagram/ouath/authorize/?client_id=<?php echo client_ID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">LOGIN</a>
+</body>
+</html>
