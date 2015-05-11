e0990e117aa4f4443d3359552f04b39a067c1900
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-05-11 10:10:55 -0700

    Milestone 4: - Added Instagram Authorization

--- index.php
+++ index.php
@@ -11,6 +11,12 @@ define('clientSecret', 'f1795294356d41468a7970fd9e4147d0');
 define('redirectURI', 'http://localhost/appacademyapi/index.php');
 define('ImageDirectory', 'pics/');
 
+if isset(($_GET['code'])) {
+	$code = ($_GET['code']);
+	$url = 'https://api.instagram.com/oauth/access_token';
+	$access_token
+}
+
  ?>
 
 <!DOCTYPE html>
