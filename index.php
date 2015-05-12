1fd96e030c60a620a3ce721e627fc21699f249fe
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-05-12 08:50:31 -0700

    Milestone 7 : - Completed Set up of cURL Options - Allows us to get Information From Instagram - Created Variable Called $result to Store Results From cURL

--- index.php
+++ index.php
@@ -18,8 +18,20 @@ if (isset($_GET['code'])) {
 								'client_secret' => clientSecret,
 								'grant_type' => 'authorization_code',
 								'redirect_uri' => redirectURI,
-								'code' => $code);
+								'code' => $code
+								);
+
+//cURL is What we Use in PHP, Its a Library Calls to Other API's
+	$curl = curl_init($url);//Setting a cURL Session and we put in $url Because Thats Where we are Getting the Date From
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings);//Setting the Post Fields to the Array Setup That we Created
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);// Setting it Equal to one Because we are Getting Strings Back
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);//but in Live Work Production we Want to set This to True
+
 }
+// stores results in $result
+$result = curl_exec($curl);
+curl_close();
 
  ?>
 
